af8d890b149369c1b9366ecd864f2a2a01a81a0c
jessieangles <jessie@angelmg.com>
2010-12-03 11:26:05 -0800

    Initial commit to savilerowlv-com.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SavilerowlvCom::Application.config.secret_token = '9462b1c55573e8fcf50568a837f86c761088d4a35fbccadc527e0f21678995c9cf8b3ee51cdfbddb26918496bef2bcec17f386ff73da5e2b9f52dfd3599a2bdc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SavilerowlvCom::Application.config.session_store :cookie_store, :key => '_savilerowlv-com_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SavilerowlvCom::Application.config.session_store :active_record_store
